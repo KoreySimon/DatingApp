e5a3315ec5e9f927b9ff4a04cbd0f5fddd683082
Korey <korey.simon@gmail.com>
2021-10-28 18:54:03 -0700

    End of section 16

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -46,7 +46,6 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        // api/users/3
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUsers(string username)
         {
